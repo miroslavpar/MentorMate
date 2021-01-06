aa221551fc8bbcaa6712fbca17cdc68b609db7a7
Miroslav Parapanov <mparapanov99@gmail.com>
2021-01-06 20:15:13 +0200

    Made some improvements with Validating ROWS and COLS Adding comments

--- Layer.py
+++ Layer.py
@@ -1,5 +1,4 @@
 class Layer:
-
     def __init__(self, rows_temp, cols_temp):
         self.max_bricks = 0
         self.rows = 0
@@ -9,28 +8,16 @@ class Layer:
         self.rectangular = []
         self.init_rectangular()
 
-# Validating ROWS and COLS
-    def validate_rows_and_cols (self, rows_temp, cols_temp):
-        return rows_temp > 0 and cols_temp > 0 and \
-               rows_temp % 2 == 0 and cols_temp % 2 == 0 and \
-               rows_temp < 100 and cols_temp < 100
-    
 # Making validation after setting their values (encapsulating the data in private fields)
-    def set_rows_and_cols(self, rows_temp, cols_temp):
-        while not self.validate_rows_and_cols(rows_temp, cols_temp):
-            print("INVALID rows and cols.")
-            print("Please enter greater than zero, not exceeding 100 and even numbers for rows and columns")
-            rows_temp = int(input("Rows: "))
-            cols_temp = int(input("Cols: "))
+    def set_rows_and_cols (self, rows_temp, cols_temp):
         # make the wall transposed
-        # it is the same
         ###########################
         #  1 1
         #  2 2   -->   1 2 3 4
         #  3 3         1 2 3 4
         #  4 4
         ###########################
-        if rows_temp > cols_temp :
+        if rows_temp > cols_temp:
             self.rows = cols_temp
             self.cols = rows_temp
         else:
@@ -41,6 +28,7 @@ class Layer:
     def init_rectangular(self):
         self.rectangular = [[0] * self.cols for _ in range(self.rows)]
 
+    # Properties but done with the old good way with getters :)
     def get_rows(self):
         return self.rows
 
@@ -50,26 +38,29 @@ class Layer:
     def get_layer(self):
         return self.rectangular
 
+    # Printing the solution
     def print(self):
         for i in range(self.rows):
             for j in range(self.cols):
                 print(self.rectangular[i][j], end=' ')
             print()
 
-    def validate_the_bricks(self, rect):
+    # Checking if bricks are validated
+    def validate_the_bricks(self):
         for i in range(self.rows):
             for j in range(self.cols):
-                if i + 2 < self.rows and rect[i][j] == rect[i + 2][j]:
+                if i + 2 < self.rows and self.rectangular[i][j] == self.rectangular[i + 2][j]:
                     print("INVALID brick. One brick is Nx1! (where N is > 2)")
                     print("Enter again the values of the matrix !")
                     return False
 
-                elif j + 2 < self.cols and rect[i][j] == rect[i][j + 2]:
+                elif j + 2 < self.cols and self.rectangular[i][j] == self.rectangular[i][j + 2]:
                     print("INVALID brick. Found a brick that is 1xM (where M is > 2)")
                     print("Enter again the values of the matrix !")
                     return False
         return True
 
+    # Entering values for the first wall and validating
     def filling_in(self):
         if self.already_set_the_matrix:
             print("It is not allowed to set another wall on the same wall ")
@@ -78,19 +69,24 @@ class Layer:
         while True:
             for i in range(self.rows):
                 for j in range(self.cols):
-                    #Validating value in cells
+                    # Validating value in cells
+                    # Stop if the user enters INVALID value and continue when the value in cell i, j is correct
                     while self.rectangular[i][j] <= 0:
                         self.rectangular[i][j] = int(input(f'[{i}][{j}]='))
                         if self.rectangular[i][j] <= 0:
                             print(f"INVALID value in position! {i} {j}")
                             print(f"Please enter greater than zero value in cell with positions {i} {j}."
-                                 "Press ENTER and then continue filling in the wall")
-            if self.validate_the_bricks(self.rectangular):
+                                  "Press ENTER and then continue filling in the wall")
+            # if first wall INVALID --> user enters again the whole wall
+            if self.validate_the_bricks():
                 break
+            else:
+                self.init_rectangular()
+                self.filling_in()
+
         self.already_set_the_matrix = True
 
-#if there is not zeros in the second wall -- > that means we found a solution
-
+    # if there is no zeros in the second wall -- > we found a solution
     def is_filled_second_layer(self):
         for i in range(self.rows):
             for j in range(self.cols):
@@ -98,61 +94,79 @@ class Layer:
                     return False
         return True
 
-    def solution(self, first, second,
+    # BACK_TRACKING !!!
+    def solution(self, first,
                  first_rows, first_cols,
                  second_rows, second_cols):
-
-        if second_cols >= self.cols - 1 and second_rows >= self.rows -1:
+        # if we filled the second layer ---> we are on the last col and last row
+        # so we return zero for TRUE
+        if second_cols >= self.cols - 1 and second_rows >= self.rows - 1:
             return 0
-
+############## REGULATION FOR FIRST WALL ##############
+        # getting on the next row if row index is out of range
         if first_cols >= self.cols:
             first_rows += 1
             first_cols = 0
-
+        # if we already found two different number --> we change the values in their cells in the first wall with -1
+        # here we skip these cells
         while first.get_layer()[first_rows][first_cols] == -1:
             first_cols += 1
             if first_cols >= self.cols:
                 first_rows += 1
                 first_cols = 0
+            if first.get_rows() >= self.rows:
+                break
+########################################################
 
-        while  second_cols >= self.cols or second_rows >= self.rows or second.get_layer()[second_rows][second_cols] != 0:
+############## REGULATION FOR SECOND WALL ##############
+        # Only if one of these is TRUE we make regulation for row or col index
+        while second_cols >= self.cols or second_rows >= self.rows or self.rectangular[second_rows][second_cols] != 0:
             if second_cols + 1 >= self.cols:
                 second_rows += 1
                 second_cols = 0
             else:
                 second_cols += 1
+            if second_cols >= self.cols or second_rows >= self.rows:
+                break
 
         if first_cols + 1 < self.cols and first.get_layer()[first_rows][first_cols] != first.get_layer()[first_rows][first_cols + 1]:
-            second.get_layer()[second_rows][second_cols] = self.max_bricks
-            second.get_layer()[second_rows][second_cols + 1] = self.max_bricks
+            self.rectangular[second_rows][second_cols] = self.max_bricks
+            self.rectangular[second_rows][second_cols + 1] = self.max_bricks
+            # Saving their values in need to be restored
             temp1 = first.get_layer()[first_rows][first_cols]
             temp2 = first.get_layer()[first_rows][first_cols + 1]
+            # make it -1 because it needs to be skipped (in the REGULATIONS)
             first.get_layer()[first_rows][first_cols] = -1
             first.get_layer()[first_rows][first_cols + 1] = -1
+            # we start with max number of brick and getting down to ZERO
             self.max_bricks -= 1
-            if self.solution(first, second, first_rows, first_cols + 2, second_rows, second_cols + 2) == -1:
-                second.get_layer()[second_rows][second_cols] = 0
-                second.get_layer()[second_rows][second_cols + 1] = 0
+            # if solution returns -1 it means that we start going back
+            # to find the best solution
+            # that's why i'm reestablishing the old values of the first wall
+            # and making the changed values of the second wall again zeros
+            if self.solution(first, first_rows, first_cols + 2, second_rows, second_cols + 2) == -1:
+                self.rectangular[second_rows][second_cols] = 0
+                self.rectangular[second_rows][second_cols + 1] = 0
                 first.get_layer()[first_rows][first_cols] = temp1
                 first.get_layer()[first_rows][first_cols + 1] = temp2
                 self.max_bricks += 1
 
         if second_rows + 1 < self.rows and first.get_layer()[first_rows][first_cols] != first.get_layer()[first_rows + 1][first_cols]:
-            second.get_layer()[second_rows][second_cols] = self.max_bricks
-            second.get_layer()[second_rows + 1][second_cols] = self.max_bricks
+            self.rectangular[second_rows][second_cols] = self.max_bricks
+            self.rectangular[second_rows + 1][second_cols] = self.max_bricks
             temp1 = first.get_layer()[first_rows][first_cols]
             temp2 = first.get_layer()[first_rows + 1][first_cols]
             first.get_layer()[first_rows][first_cols] = -1
             first.get_layer()[first_rows + 1][first_cols] = -1
             self.max_bricks -= 1
-            if self.solution( first, second, first_rows, first_cols + 1, second_rows, second_cols + 1) == -1:
-                second.get_layer()[second_rows][second_cols] = 0
-                second.get_layer()[second_rows][second_cols + 1] = 0
+            if self.solution(first, first_rows, first_cols + 1, second_rows, second_cols + 1) == -1:
+                self.rectangular[second_rows][second_cols] = 0
+                self.rectangular[second_rows][second_cols + 1] = 0
                 first.get_layer()[first_rows][first_cols] = temp1
                 first.get_layer()[first_rows + 1][first_cols] = temp2
                 self.max_bricks += 1
-                
-        if second.is_filled_second_layer():
+
+        if self.is_filled_second_layer():
             return 0
         else:
             return -1
@@ -160,4 +174,80 @@ class Layer:
 
 
 
+      # if self.already_set_the_matrix:
+      #       print("It is not allowed to set another wall on the same wall ")
+      #       return
+      #   print("Enter the first wall: ")
+      #   while True:
+      #       for i in range(self.rows):
+      #           while True
+      #               for j in input().split(' '):
+      #                try:
+      #                   num = int(j)
+      #                except ValueError:
+      #                   print("INVALID VALUE !")
+      #               if num <= 0:
+      #                   print(f"Please enter greater than zero value in cell with positions."
+      #                         "Press ENTER and then continue filling in the wall")
+
+    # def solution(self, first, second,
+    #              first_rows, first_cols,
+    #              second_rows, second_cols):
+    #
+    #     if second_cols >= self.cols - 1 and second_rows >= self.rows - 1:
+    #         return 0
+    #
+    #     if first_cols >= self.cols:
+    #         first_rows += 1
+    #         first_cols = 0
+    #
+    #     while first.get_layer()[first_rows][first_cols] == -1:
+    #         first_cols += 1
+    #         if first_cols >= self.cols:
+    #             first_rows += 1
+    #             first_cols = 0
+    #
+    #     while second_cols >= self.cols or second_rows >= self.rows or second.get_layer()[second_rows][second_cols] != 0:
+    #         if second_cols + 1 >= self.cols:
+    #             second_rows += 1
+    #             second_cols = 0
+    #         else:
+    #             second_cols += 1
+    #
+    #     if first_cols + 1 < self.cols and first.get_layer()[first_rows][first_cols] != first.get_layer()[first_rows][
+    #         first_cols + 1]:
+    #         second.get_layer()[second_rows][second_cols] = self.max_bricks
+    #         second.get_layer()[second_rows][second_cols + 1] = self.max_bricks
+    #         temp1 = first.get_layer()[first_rows][first_cols]
+    #         temp2 = first.get_layer()[first_rows][first_cols + 1]
+    #         first.get_layer()[first_rows][first_cols] = -1
+    #         first.get_layer()[first_rows][first_cols + 1] = -1
+    #         self.max_bricks -= 1
+    #         if self.solution(first, second, first_rows, first_cols + 2, second_rows, second_cols + 2) == -1:
+    #             second.get_layer()[second_rows][second_cols] = 0
+    #             second.get_layer()[second_rows][second_cols + 1] = 0
+    #             first.get_layer()[first_rows][first_cols] = temp1
+    #             first.get_layer()[first_rows][first_cols + 1] = temp2
+    #             self.max_bricks += 1
+    #
+    #     if second_rows + 1 < self.rows and first.get_layer()[first_rows][first_cols] != \
+    #             first.get_layer()[first_rows + 1][first_cols]:
+    #         second.get_layer()[second_rows][second_cols] = self.max_bricks
+    #         second.get_layer()[second_rows + 1][second_cols] = self.max_bricks
+    #         temp1 = first.get_layer()[first_rows][first_cols]
+    #         temp2 = first.get_layer()[first_rows + 1][first_cols]
+    #         first.get_layer()[first_rows][first_cols] = -1
+    #         first.get_layer()[first_rows + 1][first_cols] = -1
+    #         self.max_bricks -= 1
+    #         if self.solution(first, second, first_rows, first_cols + 1, second_rows, second_cols + 1) == -1:
+    #             second.get_layer()[second_rows][second_cols] = 0
+    #             second.get_layer()[second_rows][second_cols + 1] = 0
+    #             first.get_layer()[first_rows][first_cols] = temp1
+    #             first.get_layer()[first_rows + 1][first_cols] = temp2
+    #             self.max_bricks += 1
+    #
+    #     if second.is_filled_second_layer():
+    #         return 0
+    #     else:
+    #         return -1
 
--- main.py
+++ main.py
@@ -1,30 +1,49 @@
-#menu
 from Layer import Layer
-def mainManu():
+
+
+# menu
+def main_menu():
     print((' ' * 10) + "->  " + "WELCOME TO BRICKWORK""  <--")
-    print( "RULES: ")
+    print("RULES: ")
     print("--> Each brick is 1x2 and it can be put vertical or horizontal")
-    print( "--> Enter values which are greater than 0 and not exceeding 100 !")
-    print( "--> Size of the RECTANGULAR must be NxM where N is number of rows and M is number of columns ")
-    print( "--> N and M must be positive and even numbers and not exceeding 100 ! ")
+    print("--> Enter values which are greater than 0 and not exceeding 100 !")
+    print("--> Size of the RECTANGULAR must be NxM where N is number of rows and M is number of columns ")
+    print("--> N and M must be positive and even numbers and not exceeding 100 ! ")
+    print("--> If N is greater than M we assume the matrix to be M x N because it is the same as if we transpose N x M")
     print()
-    print( "IMPORTANT RULES : " + "!! ENJOY !!")
+    print("IMPORTANT RULES : " + "!! ENJOY !!")
     print()
+
+
+# Validating ROWS and COLS
+def validate_rows_and_cols(rows_temp, cols_temp):
+    return rows_temp > 0 and cols_temp > 0 and \
+           rows_temp % 2 == 0 and cols_temp % 2 == 0 and \
+           rows_temp < 100 and cols_temp < 100
 
 
 def main():
-    mainManu()
+    main_menu()
     print("Please enter ROWS")
-    rows = int(input("Rows: " ))
+    rows = int(input("Rows: "))
     print("Please enter COLS")
     cols = int(input("Cols: "))
+    while not validate_rows_and_cols(rows, cols):
+        print("INVALID ROWS and COLS")
+        print("Please enter greater than zero, even and not exceeding 100 numbers for ROWS and COLS")
+        print("Please enter ROWS")
+        rows = int(input("Rows: "))
+        print("Please enter COLS")
+        cols = int(input("Cols: "))
     first_layer = Layer(rows, cols)
     second_layer = Layer(rows, cols)
     first_layer.filling_in()
-    if second_layer.solution(first_layer, second_layer, 0, 0, 0, 0) == 0:
+    if second_layer.solution(first_layer, 0, 0, 0, 0) == 0:
+        print("\nThe SECOND wall should look like so:")
         second_layer.print()
     else:
         print("-1")
     return 0
+
 
 main()
